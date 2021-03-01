8b3f5452f164fb64277d8c9cb8c0c987669e3427
Shashank Deshmukh <shashank.deshmukh@prospa.com>
2021-03-01 19:01:05 +1100

    adding port in env file

--- index.js
+++ index.js
@@ -2,16 +2,18 @@ const express = require('express')
 const application = express()
 const bodyParser = require('body-parser')
 const cors = require('cors');
+
+require('dotenv/config')
 require('./connection')
+
+
 application.use(cors());
 application.options('*', cors());
-
-
 application.use(bodyParser.json())
 
 const scoreRoute = require('./routes/scores');
 application.use("/api/",scoreRoute)
 
-application.listen("8000", () =>{
+application.listen(process.env.PORT || 8000, () =>{
 	console.log("server started")
 })
